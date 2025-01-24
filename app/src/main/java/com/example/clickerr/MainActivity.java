f4a5083ecfd0d32c56cba721aa4b84976085efaa
unknown <operator@stacja42-14.szkola.zst-radom.edu.pl>
2025-01-24 13:56:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/clickerr/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.clickerr;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int points = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button clickerButton = findViewById(R.id.ClickerButton);
+        clickerButton.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        changePoints(1);
+                    }
+                }
+        );
+    }
+
+    public void changePoints(int value) {
+        points += value;
+        TextView x = findViewById(R.id.PointsTextView);
+        x.setText(points);
+    }
+
+
+}
+}
